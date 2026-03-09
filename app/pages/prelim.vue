810426b56e629700dc772436cd78d5519262f037
jimel Punzalan <jimelp012@gmail.com>
2026-03-09 21:39:26 +0800

    Add new

---
+++ app/pages/prelim.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        
+        <v-row>
+            <v-col cols="12" md="12">
+                <v-card height="200" color="#EEEEEE" rounded="xl" class="d-flex align-center justify-center">
+                    <v-card-text>
+                            <v-col cols="12" class="text-center text-h1 ">A</v-col>
+                            <v-col cols="12" class="text-center text-h4 ">10 Points</v-col>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+        
+        <v-row>
+
+            <v-col cols="12" md="9">
+              <v-row>
+
+            <v-col cols="12">
+                <v-card height="200" color="#EEEEEE" rounded="xl" class="d-flex align-center justify-center">
+                    <v-card-text>
+                            <v-col cols="12" class="text-center text-h1 ">B</v-col>
+                            <v-col cols="12" class="text-center text-h4 ">10 Points</v-col>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+
+            <v-col cols="12" md="4">
+                <v-card height="200" color="#EEEEEE" rounded="xl" class="d-flex align-center justify-center">
+                    <v-card-text>
+                            <v-col cols="12" class="text-center text-h1 ">F</v-col>
+                            <v-col cols="12" class="text-center text-h4 ">10 Points</v-col>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+
+            <v-col cols="12" md="4">
+                <v-card height="200" color="#EEEEEE" rounded="xl" class="d-flex align-center justify-center">
+                    <v-card-text>
+                            <v-col cols="12" class="text-center text-h1 ">D</v-col>
+                            <v-col cols="12" class="text-center text-h4 ">10 Points</v-col>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+
+            <v-col cols="12" md="4">
+                <v-card height="200" color="#EEEEEE" rounded="xl" class="d-flex align-center justify-center">
+                    <v-card-text>
+                            <v-col cols="12" class="text-center text-h1 ">E</v-col>
+                            <v-col cols="12" class="text-center text-h4 ">10 Points</v-col>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+
+        <v-col cols="12" md="3">
+        <v-card height="420" color="#EEEEEE" rounded="xl" class="d-flex align-center justify-center">
+          <v-card-text>
+              <v-col cols="12" class="text-center text-h1">F</v-col>
+              <v-col cols="12" class="text-center text-h4">10 Points</v-col>
+          </v-card-text>
+        </v-card>
+      </v-col>
+            
+        </v-row>
+
+    </div>
+</template>
