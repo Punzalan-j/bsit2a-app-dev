99c0e31fb0ea53036165434c873f4abefee04668
jeric <djeric291@gmail.com>
2026-04-21 10:36:30 +0800

    Added my project

---
+++ app/pages/todo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Todo App</h1>
+        <v-card>
+            <v-data-table :items="todos0" :headers="headers"></v-data-table>
+        </v-card>
+    </div>
+</template>
+<script setup>
+ const headers = [
+    { title: 'ID', key: 'id'},
+    { title: 'Title', key: 'title'},
+    { title: 'Status', key: 'completed'}
+ ]
+ const todoes = ref([])
+
+ const getTodos = async () => {
+    const data = await $fetch('https://jsonplaceholder.typicode.com/todos')
+    todoes.value = data 
+    console.log(data)
+ }
+
+ onMounted(() => {
+    getTodos();
+
+ })
+</script>
